b5c12d6476ff91ec8e5a7aa8e4430639d647865c
qingzhixing <qzsptmail@protonmail.com>
2022-05-11 01:20:18 +0800

    #nav-bar-left样式完成 提高网站图标像素

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,38 @@
 <template>
     <div id="home-page">
-        <header>Header</header>
+        <header>
+            <div id="website-info-display">
+                <router-link to="/">
+                    <img src="../assets/images/icon.png" alt="website icon" id="website-icon"/>
+                </router-link>
+                <div id="website-name">Share Your Pixel</div>
+            </div>
+            <div id="nav-bar">
+                <div id="nav-bar-left">
+                    <router-link to="/">首页</router-link>
+                    <router-link to="/discover">发现</router-link>
+                </div>
+                <div id="nav-bar-right">
+
+                </div>
+            </div>
+        </header>
         <main>Main</main>
         <footer></footer>
     </div>
 </template>
 
 <script>
+
 export default {
-    name: "HomePage"
+    name: "HomePage",
+    setup(){
+        //header
+
+        return{
+            //header
+        }
+    }
 }
 </script>
 
@@ -27,7 +51,77 @@ export default {
 header{
     width: 100%;
     height:55px;
-    background-color: #f00;
+    padding:10px;
+    box-sizing: border-box;
+
+    background-color: #434343;
+
+    color: #fff;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    #nav-bar{
+        //宽度=100%-左侧网站信息显示宽度-padding
+        width: calc(100% - 300px - 10px);
+        height: 100%;
+        padding-left: 10px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        #nav-bar-left{
+            height:100%;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            //link
+            a{
+                font-size:20px;
+                padding-left:20px;
+                text-decoration-line: none;
+                color: #fff;
+            }
+            a:hover{
+                color: #a3a3a3;
+            }
+            a:active{
+                color: #757575;
+            }
+        }
+    }
+
+    #website-info-display{
+        background-color: #5a4f4f;
+
+        width:300px;
+        height:100%;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: left;
+        align-items: center;
+
+        border-radius:20px;
+
+        #website-icon{
+            padding-left: 10px;
+            width: 70px;
+            height: 70px;
+        }
+        #website-name{
+            //margin-left: 10px;
+            width: 100%;
+            text-align: center;
+
+            font-size: 20px;
+        }
+    }
+
 }
 main{
     width: 100%;
